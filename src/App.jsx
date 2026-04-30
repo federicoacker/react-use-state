4086d03f0c8e027fcb39462846617af4461c7630
federicoacker <acker.federico@gmail.com>
2026-04-30 13:45:50 +0200

    Messo i components dentro app, vedrò se riesco a farlo funzionare

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
-import Container from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container.jsx";
+import Header from "./components/Header.jsx";
+import Buttons from "./components/Buttons.jsx";
+import LanguageCard from "./components/LanguageCard.jsx";
+import languages from "./utils/languages.js";
 
 function App() {
   return (
     <>
-    <h1>Learn Web Development</h1>
+    <Header/>
     <Container>
-
+        <Buttons languages={languages}/>
+        <LanguageCard/>
     </Container>
     </>
   );
